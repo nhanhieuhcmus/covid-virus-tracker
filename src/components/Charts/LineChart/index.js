d71670699a308309fd0906a467822b67348fadf4
nhanhieuhcmus <vosihieunhan@gmail.com>
2021-07-27 17:44:02 +0700

    Format x-axis into correct date

---
+++ src/components/Charts/LineChart/index.js
@@ -0,0 +1,68 @@
+import HighchartsReact from "highcharts-react-official";
+import Highchart from "highcharts";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+
+const generateOptions = (data) => {
+    const formatedDate = data.map(item=>moment(item.Date).format('DD/MM/YYYY'));
+    return {
+        chart: {
+            height: 500,
+        },
+        title: {
+            text: "Number of confirmed",
+        },
+        xAxis: {
+            categories: formatedDate,
+            crosshair: true,
+        },
+        colors: ["#F3585B"],
+        yAxis: {
+            min: 0,
+            title: {
+                text: null,
+            },
+            labels: {
+                align: "right",
+            },
+        },
+        tooltip: {
+            headerFormat:
+                '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat:
+                '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y} ca</b></td></tr>',
+            footerFormat: "</table>",
+            shared: true,
+            useHTML: true,
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0,
+            },
+        },
+        series: [
+            {
+                name: "Number of confirmed",
+                data: data.map((item) => item.Confirmed),
+            },
+        ],
+    };
+};
+
+function LineChart({data}) {
+    const [options, setOptions] = useState({});
+    useEffect(()=>{
+        setOptions(generateOptions(data));
+    },[data]);
+
+
+    return (
+        <div>
+            <HighchartsReact highcharts={Highchart} options={options} />
+        </div>
+    );
+}
+
+export default LineChart;
