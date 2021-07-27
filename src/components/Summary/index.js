d0f82ecd298b1bce31eb0fb247b96ecc67f7436c
nhanhieuhcmus <vosihieunhan@gmail.com>
2021-07-27 17:41:58 +0700

    Done Summary/ LineChart component

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -1,10 +1,15 @@
+import { Grid } from '@material-ui/core'
 import React from 'react'
+import LineChart from '../Charts/LineChart'
 
-function Summary() {
+function Summary({report}) {
     return (
-        <div>
-            Summary
-        </div>
+        <Grid container spacing={3}>
+            <Grid item sm={8} xs={12}>
+                <LineChart data={report}/>
+            </Grid>
+            <Grid item sm={4} xs={12}></Grid>
+        </Grid>
     )
 }
 
