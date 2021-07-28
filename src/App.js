dd8e52c416ad67553466f91438dc595c037d65bc
nhanhieuhcmus <vosihieunhan@gmail.com>
2021-07-28 13:56:41 +0700

    Add Autocomplete from MaterialUI. Select country both typing or clicking.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { getCountries, getReportByCountry } from "./apis";
 import CountrySelector from "./components/CountrySelector";
 import Highlight from "./components/Highlight";
 import Summary from "./components/Summary";
-import '@fontsource/roboto';
+import "@fontsource/roboto";
+import MultiCountrySearch from "./components/MultiCountrySearch";
 
 function App() {
     const [countries, setCountries] = useState([]);
+    const [selectedCountry, setSelectedCountry] = useState("");
     const [selectedCountryId, setSelectedCountryId] = useState("");
     const [report, setReport] = useState([]);
 
@@ -21,19 +23,37 @@ function App() {
         });
     }, []);
 
-    const handleOnChange = (event) => {
-        setSelectedCountryId(event.target.value);
-        console.log("User click: ", event.target.value);
+    // const handleOnChange = (event) => {
+    //     setSelectedCountryId(event.target.value);
+    //     console.log("User click: ", event.target.value);
+    // };
+
+    // for new search button
+    const handleChange = (event, value) => {
+        if (value) {
+            setSelectedCountryId(value.ISO2.toLowerCase());
+            console.log("event: ", event.target.value);
+            console.log("value: ", value.ISO2.toLowerCase());
+        }
+        // setSelectedCountryId("User click new search button: ", value.ISO2.toLowerCase());
     };
 
     useEffect(() => {
         if (selectedCountryId) {
-            const { Slug } = countries.find(
+            // const { Slug } = countries.find(
+            //     (country) => country.ISO2.toLowerCase() === selectedCountryId
+            // );
+
+            const selectedCountry = countries.find(
                 (country) => country.ISO2.toLowerCase() === selectedCountryId
             );
+              
+              setSelectedCountry(selectedCountry.Country);
+
             // call API
-            getReportByCountry(Slug).then((res) => {
+            getReportByCountry(selectedCountry.Slug).then((res) => {
                 // delete the last item because it has not updated till end of the day
+                console.log("getReportByCountry: ", res);
                 res.data.pop();
                 setReport(res.data);
             });
@@ -41,15 +61,20 @@ function App() {
     }, [countries, selectedCountryId]);
 
     return (
-        <Container style={{marginTop:20}}>
+        <Container style={{ marginTop: 20 }}>
             <Typography variant="h2" component="h2">
                 COVID-19 TRACKER
             </Typography>
             <Typography>{moment().format("LLL")}</Typography>
-            <CountrySelector
+            {/* <CountrySelector
                 value={selectedCountryId}
                 countries={countries}
                 handleOnChange={handleOnChange}
+            /> */}
+            <MultiCountrySearch
+                // value={selectedCountry}
+                countries={countries}
+                handleChange={handleChange}
             />
             <Highlight report={report} />
             <Summary report={report} />
--- src/components/Highlight/HighlightCard.js
+++ src/components/Highlight/HighlightCard.js
@@ -5,10 +5,10 @@ import React from "react";
 const useStyles = makeStyles({
     wrapper: (props) => {
         if (props.type === "confirmed")
-            return { borderLeft: "5px solid #c9302c" };
+            return { borderLeft: "5px solid rgba(255,0,0,0.5)" };
         if (props.type === "recovered")
-            return { borderLeft: "5px solid #28a745" };
-        else return { borderLeft: "5px solid gray" };
+            return { borderLeft: "5px solid rgba(0,255,0,0.5)" };
+        else return { borderLeft: "5px solid rgba(128,128,128,0.5)" };
     },
     title: { fontSize: 18, marginBottom: 5 },
     count: { fontWeight: "bold", fontSize: 18 },
@@ -16,6 +16,7 @@ const useStyles = makeStyles({
 
 function HighlightCard({ title, count, type }) {
     const styles = useStyles({ type });
+    if (!count) return "Loading ...";
     // console.log("HighlightCard: ", {title,count,type});
     return (
         <Card className={styles.wrapper}>
