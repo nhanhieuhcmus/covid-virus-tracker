f1205e8b5f5dc8156007ee49627602dec3d18d77
nhanhieuhcmus <vosihieunhan@gmail.com>
2021-08-03 10:48:38 +0700

    Logic change languages.

---
+++ src/languages/languages.js
@@ -0,0 +1,76 @@
+export default {
+    "VN": {
+        "CountryPicker": {
+            "label": "Chọn quốc gia ...",
+        },
+        "Highlight": {
+            "Confirmed": "Nhiễm bệnh",
+            "Recovered": "Phục hồi",
+            "Deaths": "Tử vong"
+        },
+        "Summary": {
+            "select": {
+                "all": "Tất cả",
+                "_30": "30 ngày gần nhất",
+                "_7": "7 ngày gần nhất"
+            },
+            "label": {
+                "Confirmed": "Nhiễm bệnh",
+                "Recovered": "Phục hồi",
+                "Deaths": "Tử vong"
+            }
+        },
+        "StatTable": {
+            "title": "Thống kê theo quốc gia",
+            "Country": "Quốc gia",
+            "Confirmed": "Số ca nhiễm",
+            "Recovered": "Số ca phục hồi",
+            "Deaths": "Số ca tử vong"
+        },
+        "VaccineTable": {
+            "Province": "Tỉnh",
+            "Expected": "Phân bổ dự kiến",
+            "Real": "Phân bổ thực tế",
+            "Population18": "Dân số >= 18t",
+            "Injected": "Số liều đã tiêm",
+            "ExpectedRate": "Tỉ lệ dự kiến/ dân số (>= 18t)",
+            "InjectedRate": "Tỉ lệ thực tế/ dân số (>= 18t)",
+            "Injected1": "Tỉ lệ tiêm >= 1 mũi (>= 18t)"
+        },
+        "ScrollTop": {
+            "innerText": "Trang chủ"
+        }
+    },
+    "EN": {
+        "CountryPicker": {
+            "label": "Select country ...",
+        },
+        "Highlight": {
+            "Confirmed": "Confirmed",
+            "Recovered": "Recovered",
+            "Deaths": "Deaths"
+        },
+        "Summary": {
+            "select": {
+                "all": "All the time",
+                "_30": "The last 30 days",
+                "_7": "The last 7 days"
+            },
+            "label": {
+                "Confirmed": "Confirmed",
+                "Recovered": "Recovered",
+                "Deaths": "Deaths"
+            }
+        },
+        "StatTable": {
+            "title": "Statistics by country",
+            "Country": "Country",
+            "Confirmed": "Confirmed",
+            "Recovered": "Recovered",
+            "Deaths": "Deaths"
+        },
+        "ScrollTop": {
+            "innerText": "Home"
+        }
+    }
+}
